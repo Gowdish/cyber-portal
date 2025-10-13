05b766c7a97a32029e856e3ae33e96a2efd747ef
Gowdish <165966232+Gowdish@users.noreply.github.com>
2025-10-13 23:34:28 +0530

    Update lid.py

--- lid.py
+++ lid.py
@@ -1510,4 +1510,4 @@ if __name__ == '__main__':
     print("=" * 80 + "\n")
     
     # NOTE: Set debug=False and host='0.0.0.0' for production environment.
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
